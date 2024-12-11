bb3ba06ebef41ffd27006826c4c77b3ef60c6b8c
FreeAhuraBird <131903454+FreeAhuraBird@users.noreply.github.com>
2024-12-11 20:02:48 +0100

    new implementations in gamemode_playerrelativity.h
    	FString GetPlayerRelativity(const AActor* TargetActor);
    	FString DetermineQuadrant(const FVector& RelativeVector);
    	float CalculateAngle(const FVector& RelativeVector);

---
+++ src/unreal_engine/gamemode_playerrelativity.h
@@ -0,0 +1,44 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "HttpHandler_Get.h"
+#include "GameFramework/GameModeBase.h"
+#include "projectGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AprojectGameMode : public AGameModeBase
+{
+    GENERATED_BODY()
+
+public:
+    AprojectGameMode();
+	FTimerHandle TimerHandle;
+	void GetActors();
+	void PerformTracking();
+	void TickForGetWorld();
+	FString GetPlayerRelativity(const AActor* TargetActor);
+	FString DetermineQuadrant(const FVector& RelativeVector);
+	float CalculateAngle(const FVector& RelativeVector);
+	//void SendPayload(FString Payload);
+
+
+	struct FTrackedObject {
+		AActor* Actor;
+		FString ActorName;
+		FVector PreviousPosition;
+		bool IsPlayer;
+	};
+
+	TArray<FTrackedObject> TrackedObjects;
+
+	UHttpHandler_Get* HttpHandler;
+	void httpSendReq(FString *PayloadJson);
+
+
+protected:
+    // beginplay is overriden when the functions above are called
+    virtual void BeginPlay() override;
+};
+
