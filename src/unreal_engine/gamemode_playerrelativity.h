58621c65c774d541b257600aaadc43021c21a254
FreeAhuraBird <131903454+FreeAhuraBird@users.noreply.github.com>
2024-12-23 18:33:12 +0100

    Update gamemode_playerrelativity.h

--- src/unreal_engine/gamemode_playerrelativity.h
+++ src/unreal_engine/gamemode_playerrelativity.h
@@ -13,36 +13,52 @@ class AprojectGameMode : public AGameModeBase
     GENERATED_BODY()
 
 public:
+	// constructor
     AprojectGameMode();
-	FTimerHandle TimerHandle;
+
+	// World tracking
+	void TickForWholeWorld();
+	void WholeWorldJson();
+
+	// Actor tracking
+	void TickForGetWorld();
 	void GetActors();
 	void PerformTracking();
-	void TickForGetWorld();
-	void WholeWorldJson();
+
+	// for serialization
 	TSharedPtr<FJsonObject> SerializeVector(const FVector& Vector);
+
+	// for determine relativity
 	void GetPlayerRelativity(const AActor* TargetActor);
-	void SendPayload(const FString& Payload, const FString& indicator);
-	FString question_prompt(const FString& indicator);
 	FString DetermineQuadrant(const FVector& RelativeVector);
 	float CalculateAngle(const FVector& RelativeVector);
 	FString GetRelativePosition(const float& ForwardDot, const float& RightDot, const float& VerticalDot);
 
+
+	// HHTP commication
+	void SendPayload(const FString& Payload, const FString& indicator, const FString& context);
+	FString question_prompt(const FString& indicator);
+	UHttpHandler_Get* HttpHandler;
+	void httpSendReq(FString Payload, FString question, FString context); 
+
+	// Example variable for Blueprint access
+	UPROPERTY(BlueprintReadWrite, Category = "CustomLogic")
+	int32 Score;
+
+private:
+	// Timer
+	FTimerHandle TimerHandle;
+
+	// Tracked objects struct
 	struct FTrackedObject {
 		AActor* Actor;
 		FString ActorName;
 		FVector PreviousPosition;
 		bool IsPlayer;
 	};
-
+	
+	// Tracked objects array using the struct above
 	TArray<FTrackedObject> TrackedObjects;
-
-	UHttpHandler_Get* HttpHandler;
-	void httpSendReq(FString *PayloadJson);
-
-	// Example variable for Blueprint access
-	UPROPERTY(BlueprintReadWrite, Category = "CustomLogic")
-	int32 Score;
-
 
 protected:
     // beginplay is overriden when the functions above are called
